839a6e0d30fe54809d640de06c78b9e467133bc6
wuxiaohui <aipdx@qq.com>
2021-01-19 17:58:52 +0800

    feat:'我的'页面

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,66 +1,27 @@
-// pages/user/user.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userInfo: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    this.getStorageUserInfo()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getUserInfo(e) {
+    const {userInfo} = e.detail
+    console.log(userInfo)
+    this.setData({userInfo})
+    wx.setStorageSync('userInfo', userInfo)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getStorageUserInfo() {
+    const userInfo = wx.getStorageSync('userInfo')
+    console.log(userInfo)
+    if (userInfo) {
+      this.setData({userInfo})
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 联系客服
+  freeTell() {
+    wx.makePhoneCall({
+      phoneNumber: '400-618-4000'
+    })
   }
 })
