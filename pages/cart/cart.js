8719b3962287ecbdcc69682827e9ec1896d25436
wuxiaohui <aipdx@qq.com>
2021-01-19 13:52:42 +0800

    fix:购物车页面代码优化

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -37,7 +37,7 @@ Page({
   },
   // 获取加入购物车的商品
   getCartProduct() {
-    const cartList = wx.getStorageSync('cart')
+    const cartList = wx.getStorageSync('cart') || []
     if (cartList) {
       this.getTotal(cartList)
       this.setData({
@@ -45,36 +45,50 @@ Page({
       })
     }
   },
-  // 单个勾选框事件
+  // 单个勾选框事件---修改data和缓存里的值
   singleCheck(e) {
     const {checked, index} = e.currentTarget.dataset
     const {cartList} = this.data
     cartList[index].checked = !checked
     this.getTotal(cartList)
+    wx.setStorageSync('cart', cartList)
     this.setData({
       cartList
     })
   },
+  // 计算总价格，买的总数量，是否全选
   getTotal(cartList) {
     if (Array.isArray(cartList) && cartList.length) {
-      // 全选状态
-      const checkedAll = cartList.every(v => v.checked)
-      // 总价格
-      const totalPrice = cartList.reduce((pre, next) => {
-        if (next.checked) {
-          return (pre + next.goods_price * next.num)
-        } else {
-          return pre
+      // 全选状态 当数组为空时，使用every会返回true,所以先判断下数组的长度
+      // 循环太多，统一用forEach
+      // let checkedAll = cartList.every(v => v.checked)
+      // let totalPrice = cartList.reduce((pre, next) => {
+      //   if (next.checked) {
+      //     return (pre + next.goods_price * next.num)
+      //   } else {
+      //     return pre
+      //   }
+      // }, 0)
+      // // 结算总数
+      // let buyTotal = cartList.reduce((pre, next) => {
+      //   if (next.checked) {
+      //     return (pre + next.num)
+      //   } else {
+      //     return pre
+      //   }
+      // }, 0)
+      let checkedAll = true
+      // 总价格 结算总数
+      let totalPrice = 0
+      let buyTotal = 0
+      cartList.forEach(val => {
+        if (val.checked) {
+          totalPrice += val.goods_price * val.num
+          buyTotal += val.num
+        } else { // 如果有一个为false，则不是全选
+          checkedAll = false
         }
-      }, 0)
-      // 结算总数
-      const buyTotal = cartList.reduce((pre, next) => {
-        if (next.checked) {
-          return (pre + next.num)
-        } else {
-          return pre
-        }
-      }, 0)
+      })
       this.setData({
         checkedAll,
         totalPrice,
@@ -86,64 +100,86 @@ Page({
   changeBuyNum(e) {
     const {num, index} = e.currentTarget.dataset
     const {cartList} = this.data
-    cartList[index].num += num
-    if (cartList[index].num <= 0) {
-      cartList[index].num = 0
+    // 数量为1并且点击的是减号
+    if (cartList[index].num === 1 && num === -1) {
       wx.showModal({
+        title: '提示',
         content: '是否删除该商品?',
         showCancel: true,
         success: res => {
           if (res.confirm) {
             cartList.splice(index, 1)
+            if (cartList.length < 1) {
+              // 如果商品全部删除，则把全选改为false
+              this.setData({
+                checkedAll: false
+              })
+            }
+            this.getTotal(cartList)
             this.setData({
               cartList
             })
             wx.setStorageSync('cart', cartList)
           } else if (res.cancel) {
-            cartList[index].num = 1
-            this.setData({
-              cartList
-            })
-            wx.setStorageSync('cart', cartList)
+            console.log('点击了取消')
           }
         }
       })
+    } else {
+      cartList[index].num += num
+      this.getTotal(cartList)
+      this.setData({
+        cartList
+      })
     }
-    this.getTotal(cartList)
-    this.setData({
-      cartList
-    })
   },
   // 全选
   handleCheckAll() {
     const {checkedAll, cartList} = this.data
-    if (checkedAll) {
-      cartList.forEach(value => value.checked = false)
+    if (Array.isArray(cartList) && cartList.length) {
+      cartList.forEach(value => value.checked = !checkedAll)
+      this.getTotal(cartList)
       this.setData({
-        checkedAll: false
+        cartList,
+        checkedAll: !checkedAll
       })
-    } else {
-      cartList.forEach(value => value.checked = true)
-      this.setData({
-        checkedAll: true
-      })
+      wx.setStorageSync('cart', cartList)
     }
-    this.getTotal(cartList)
-    this.setData({
-      cartList
-    })
   },
   // 结算
   closeAccount() {
-    const goodsIds = []
-    const {cartList,totalPrice,buyTotal} = this.data
+    const {cartList, totalPrice, buyTotal, address} = this.data
+    if (!cartList.length) {
+      wx.showToast({
+        title: '您还没有选购商品哦',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    if (!address.userName) {
+      wx.showToast({
+        title: '您还没有添加收货地址',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    const payOrder = {
+      payList: []
+    }
+    // 获取勾选的商品
     cartList.forEach(value => {
       if (value.checked) {
-        goodsIds.push(value.goods_id)
+        payOrder.payList.push(value)
       }
     })
-    console.log('总金额：', totalPrice)
-    console.log('总件数：', buyTotal)
-    console.log('勾选的ID：', goodsIds)
+    payOrder.totalPrice = totalPrice
+    payOrder.buyTotal = buyTotal
+    wx.setStorageSync('payOrder', payOrder)
+    // 跳转到支付页面
+    wx.navigateTo({
+      url: '/pages/pay/pay'
+    })
   }
 })
