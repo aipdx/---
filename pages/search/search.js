4356cc1b5d5cd8ba2c8cc0310fc134094e83f976
wuxiaohui <aipdx@qq.com>
2021-01-20 11:31:30 +0800

    feat:搜索功能

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,66 +1,49 @@
-// pages/search/search.js
+import {request} from '../../request/index'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    keyword: '',
+    searchList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  TimeId: -1,
+  onLoad(options) {
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // input的bindinput事件如果用async回显示错误，绑定的该方法不能使用异步，异步请求方法放外边
+  handleSearch(e) {
+    const {value} = e.detail
+    if (!value.trim()) return
+    clearTimeout(this.TimeId)
+    this.TimeId = setTimeout(() => {
+      this.qsearch(value)
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  async qsearch(query) {
+    try {
+      const params = {
+        url: '/goods/qsearch',
+        data: {
+          query
+        }
+      }
+      const res = await request(params)
+      this.setData({
+        searchList: res,
+        keyword: query
+      })
+    } catch (e) {
+      console.log(e)
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 清楚搜索内容
+  clearInput() {
+    this.setData({
+      keyword: ''
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 取消搜索 -- 返回上一页
+  cancelSearch() {
+    wx.navigateBack({
+      delta: 1
+    })
   }
 })
