54357a496c0c37926db47ad2e8b81e6745d12a14
wuxiaohui <aipdx@qq.com>
2021-01-20 11:43:05 +0800

    fix:搜索功能优化

--- pages/search/search.js
+++ pages/search/search.js
@@ -11,7 +11,14 @@ Page({
   // input的bindinput事件如果用async回显示错误，绑定的该方法不能使用异步，异步请求方法放外边
   handleSearch(e) {
     const {value} = e.detail
-    if (!value.trim()) return
+    if (!value.trim()) {
+      clearTimeout(this.TimeId)
+      this.setData({
+        searchList: [],
+        keyword: ''
+      })
+      return
+    }
     clearTimeout(this.TimeId)
     this.TimeId = setTimeout(() => {
       this.qsearch(value)
@@ -40,10 +47,17 @@ Page({
       keyword: ''
     })
   },
-  // 取消搜索 -- 返回上一页
+  // 取消搜索 -- 返回上一页或者清空输入的内容和数据
   cancelSearch() {
-    wx.navigateBack({
-      delta: 1
+    // 返回上一页
+    // wx.navigateBack({
+    //   delta: 1
+    // })
+
+    // 重置
+    this.setData({
+      keyword: '',
+      searchList: []
     })
   }
 })
