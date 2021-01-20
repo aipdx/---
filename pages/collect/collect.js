4c545e54fc812fb31346a91d10b6e68ab7a25309
wuxiaohui <aipdx@qq.com>
2021-01-20 09:31:21 +0800

    feat:收藏商品列表数据渲染

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -1,66 +1,35 @@
-// pages/collect/collect.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabsList: [
+      {id: 0, name: '商品收藏', checked: true},
+      {id: 1, name: '品牌收藏', checked: false},
+      {id: 2, name: '店铺收藏', checked: false},
+      {id: 3, name: '浏览足迹', checked: false},
+    ],
+    collectList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 用onShow好些，因为如果从收藏点入到详情，取消收藏后再返回，数据要变化
+  onShow() {
+    this.getCollectList()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取收藏列表
+  getCollectList() {
+    const collectList = wx.getStorageSync('collectGoods') || []
+    if (collectList.length) {
+      this.setData({
+        collectList
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleTabs(e) {
+    const index = e.detail
+    this.changeTabsByIndex(index)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // tabs状态修改公共代码
+  changeTabsByIndex(index) {
+    const {tabsList} = this.data
+    tabsList.forEach(v => v.checked = false)
+    tabsList[index].checked = true
+    this.setData({tabsList})
   }
 })
