c013ef31d56aa9ffaf4a642d6905fac12219b3ce
wuxiaohui <aipdx@qq.com>
2021-01-22 14:28:23 +0800

    fix:新增地址和修改地址使用weui重新

--- pages/address_manage/address_manage.js
+++ pages/address_manage/address_manage.js
@@ -7,8 +7,25 @@ Page({
       addrDetail: '',
       region: ['请选择省市区/县', '', ''], // 省市区数据 第一个可以当placeholder
     },
+    errorMsg: '', // 验证表单显示错误信息
     customItem: '', // 自定义picker显示的内容
+    /*========================================weui==================================================*/
+    rules: [
+      {
+        name: 'userName',
+        rules: {required: true, message: '请填写收货人姓名'},
+      },
+      {
+        name: 'telNumber',
+        rules: [{required: true, message: '请填写收货人电话'}, {mobile: true, message: '电话格式不对'}]
+      },
+      {
+        name: 'addrDetail',
+        rules: {required: true, message: '请填写详细地址'}
+      },
+    ],
   },
+  externalClasses: ['form_item','form_item_region'],
   Postcode: '', // 邮编
   AddressId: -1,
   onShow() {
@@ -82,7 +99,7 @@ Page({
       })
       return false
     }
-    if (!region.length) {
+    if (region.length.length < 2) {
       wx.showToast({
         title: '请先选择省市区',
         icon: "none"
@@ -115,10 +132,67 @@ Page({
       'form.userName': '',
       'form.telNumber': '',
       'form.addrDetail': '',
-      'form.region': []
+      'form.region': ['请选择省市区/县', '', '']
     })
     wx.navigateBack({
       delta: 1
+    })
+  },
+  /*========================================weui==================================================*/
+  formInputChange(e) {
+    const {field} = e.currentTarget.dataset
+    this.setData({
+      [`form.${field}`]: e.detail.value
+    })
+  },
+  // weui提交表单
+  weSubmitForm() {
+    const {userName, telNumber, region, addrDetail} = this.data.form
+    const validRegion = region.filter(v => v) // 提取有效值
+    this.selectComponent('#form').validate((valid, errors) => {
+      if (!valid) {
+        const firstError = Object.keys(errors)
+        if (firstError.length) {
+          this.setData({
+            errorMsg: errors[firstError[0]].message
+          })
+        }
+      } else if (validRegion.length < 2) {
+        this.setData({
+          errorMsg: '请选择省市区'
+        })
+      } else {
+        const addressList = wx.getStorageSync('addressList') || []
+        if (this.AddressId >= 0) { // 编辑地址
+          addressList[this.AddressId].userName = userName
+          addressList[this.AddressId].telNumber = telNumber
+          addressList[this.AddressId].postalCode = this.Postcode
+          addressList[this.AddressId].provinceName = region[0]
+          addressList[this.AddressId].cityName = region[1]
+          addressList[this.AddressId].countyName = region[2]
+          addressList[this.AddressId].detailInfo = addrDetail
+          addressList[this.AddressId].all = region[0] + region[1] + region[2] + addrDetail
+        } else { // 新增地址
+          const address = {
+            userName,
+            telNumber,
+            postalCode: this.Postcode,
+            provinceName: region[0],
+            cityName: region[1],
+            countyName: region[2],
+            detailInfo: addrDetail,
+            all: region[0] + region[1] + region[2] + addrDetail
+          }
+          addressList.push(address)
+        }
+        wx.setStorageSync('addressList', addressList)
+        wx.showToast({
+          title: '提交成功',
+        })
+        wx.navigateBack({
+          delta: 1
+        })
+      }
     })
   }
 })
